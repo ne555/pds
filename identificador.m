a46f046de127eeed82c0bb1652981ed25168f82c
aldo_bedrij <wbedrij@gmail.com>
2013-06-09 15:41:13 -0300

    ventaneo en todo el tiempo

--- identificador.m
+++ identificador.m
@@ -1,43 +1,70 @@
 source funciones.m;
+source ./mfcc/melfcc.m
+inicio = [14,12,12,12,13,20,12,12,20,7]'*1e3;
 
-[signal,fs,bps] = wavread('./grabaciones/all/8/69.wav');
-
-%figure(3);
-%plot(x);
-%figure(4);
-%plot(y);
-
-inicio = 12e3;
-n = 2*1024;
-x = signal(inicio:inicio+n-1) .* hanning(n);
-
-%como es un hombre tomar f0~200hz
-muestras = floor(fs/200);
-tx = liftrado(x,muestras);
-
-%prediccion lineal
-[a,err] = prediccion_lineal(x,14);
-orden = 14;
-a = a(:,orden);
-freq = freqz( sqrt(err(orden)), [1;a],n,'whole');
-
-df = fs/n;
-f = [0:df:fs-df];
+vocales = [800 1600; 600 2500; 300 2500; 600 800; 300 800];
+debe_dar = [vocales(4,:); vocales(1,:); vocales(2,:); vocales(3,:); vocales(4,:); vocales(5,:); vocales(1,:); vocales(2,:); vocales(3,:); vocales(4,:)];
 
 
-%por supuesto la culpa es de tu lsp
-r = lsp2(a);
-r2 =unique(abs(r))*fs/(2*pi);
 
+for digito=[8]
+	for L=[0:6]
+	valores = [];
+	tracto = [];
+	lsp_coef = [];
+		archivo = sprintf('./grabaciones/all/%d/%d.wav',digito,10*L+digito+1);
+		[signal,fs,bps] = wavread(archivo);
 
-figure(1);
-clf;
-%plot(f,abs(tx), 'k');
-hold on;
-plot(f,abs(freq), 'r');
-stem(r2, 5*ones(size(r2)), 'k' );
+		n = 1024;
+		[frames,t] = ventaneo(signal, n, 2, hanning(n));
 
-figure(2);
-plot(x);
+		for K=[1:size(frames)(2)]
+			x = frames(:,K);
+			%prediccion lineal
+			[a,err] = prediccion_lineal(x,14);
+			orden = 14;
+			a = a(:,orden);
 
-r2'
+			%por supuesto la culpa es de tu lsp
+			r = lsp2(a);
+			r2 =unique(abs(r))*fs/(2*pi);
+
+			valores = [valores;r2'];
+
+			tracto = [tracto,a];
+			lsp_coef = [lsp_coef,r2];
+		end
+
+		clf;
+		subplot(2,1,2);
+		hold on;
+		for K=[2:4]
+			plot( t,lsp_coef(K,:), sprintf('-*%d',K-1) );
+		end
+		subplot(2,1,1);
+		plot(signal);
+
+		pause;
+	end
+end
+
+%df = fs/n;
+%f = [0:df:fs-df];
+%f = f(1:n/2);
+%for digito=[0,4,9]
+%
+%	freq = freqz(1, [1;tracto(:,digito+1)],n,'whole');
+%	freq = freq(1:n/2);
+%
+%	digito
+%	figure(1);
+%	clf;
+%	%plot(f,abs(tx), 'k');
+%	hold on;
+%	plot(log(f),log(abs(freq)), 'r');
+%	stem(log(lsp_coef(:,digito+1)), 2*ones(size(lsp_coef(:,digito+1))), 'k' );
+%	stem(log(lsp_coef(:,digito+1)), -10*ones(size(lsp_coef(:,digito+1))), 'k' );
+%	stem( log(debe_dar(digito+1,:)), 10*ones(size(debe_dar(digito+1,:))), 'b');
+%	stem( log(debe_dar(digito+1,:)), -10*ones(size(debe_dar(digito+1,:))), 'b');
+%	pause;
+%end
