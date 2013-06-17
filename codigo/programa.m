feccd8661f7cb3916b47b670619929f2142c6e1a
aldo_bedrij <wbedrij@gmail.com>
2013-06-17 12:10:42 -0300

    conclusion

--- codigo/programa.m
+++ codigo/programa.m
@@ -6,10 +6,10 @@ for digito=[0:9]
 	for L=[0:9]
 		lsp_coef = [];
 		%archivo = sprintf('../grabaciones/all/%d/%d.wav',digito,L);
-		archivo = sprintf('../grabaciones/seleccionados/%d/%d.wav',digito,L);
+		archivo = sprintf('../grabaciones/prueba/%d/%d.wav',digito,L);
 		[signal,fs,bps] = wavread(archivo);
 		signal = signal(:,1);
-		signal = agregar_ruido(signal, 0);
+		signal = agregar_ruido(signal, 20);
 		vocal = dame_la_vocal(signal,fs, 8);
 
 		clase = clasificador_estadistico(signal,vocal, digito);
